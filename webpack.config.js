4b52249a6d62c5666be985ba029ad4f4a90772ff
Dzenan <ddevedzic1@etf.unsa.ba>
2021-01-23 14:33:12 +0100

    Added routing support

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,7 @@ module.exports = {
   output: {
     path: settings.distPath,
     filename: "bundle.js",
+    publicPath: "/",
   },
   module: {
     rules: [
@@ -33,6 +34,9 @@ module.exports = {
         use: ["style-loader", "css-loader"],
       },
     ],
+  },
+  devServer: {
+    historyApiFallback: true,
   },
   plugins: [
     new HtmlWebPackPlugin({
