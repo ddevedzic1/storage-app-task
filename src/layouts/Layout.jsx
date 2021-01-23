772407b25c2fd790dc2174a0a5aaca47b8e592a4
Dzenan <ddevedzic1@etf.unsa.ba>
2021-01-23 14:59:30 +0100

    Added BucketListPage

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import ErrorPage from "../pages/ErrorPage";
 import { Switch, Route } from "react-router-dom";
+import BucketListPage from "../pages/BucketListPage"
 
 const Layout = () => {
     return (
         <div>
             <Navbar />
             <Switch>
+                <Route path="/" exact component={BucketListPage} />
                 <Route component={ErrorPage} />
             </Switch>
         </div>
---
+++ src/pages/BucketListPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const BucketListPage = () => {
+    return (
+        <div>
+            <h3 style={{ marginLeft: "1em", marginTop: "1em" }}>Bucket list</h3>
+        </div>)
+
+}
+
+export default BucketListPage;
